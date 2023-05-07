16566143f8c5b4dbd825981598ec2345a02464dd
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-05-06 22:21:12 -0700

    need to start try-catch

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const weatherData = require('')
+const weatherData = require('./data/weather.json')
 
 // initializing express
 const app = express();
@@ -11,40 +11,42 @@ const app = express();
 // middleware to allow open access with cors
 app.use(cors())
 
+
 const PORT = process.env.PORT;
 
-app.get()('/', (request, response) => {
+app.get('/', (request, response) => {
   response.status(200).send('Hey your default route is working')
 });
 
 // static date from lecture, probably not applicable
 // NEXT param is for catch block
-app.get('/shoppingList', (request, response, next) => {
+app.get('/weather', (request, response, next) => {
   console.log(request.query);
-  const { listName, name } = request.query
-  console.log(listName, name);
-  response.status(200).send(shoppingListData);
+  // const { lat, lon, searchQuery } = request.query
+  const selectCity = weatherData.find(city => city.city_name === searchQuery)
+  const formattedData = selectCity.data.map(day => new Forecast(day))
+  response.status(200).send(formattedData);
 });
 
 // make edits according to the assignment
 // this is for formatting the data
-class List {
+class Forecast {
   constructor(obj) {
-    this.name = obj.name;
-    this.description = obj.description
+    this.date = obj.datetime;
+    this.description = obj.weather.description
   }
 }
 
-app.get('*', (req, res) => {
-  res.status(500).send(error.message);
-})
+// app.get('*', (req, res) => {
+//   res.status(500).send(error.message);
+// })
 
 
 // fix this function -------------------
-app.use('error, req, res, next') => {
-  res.status.(500).send(error.message);
-})
+// app.use('error, req, res, next') => {
+//   res.status.(500).send(error.message);
+// })
 
 
 
-app.listen(PORT, () => console.log('listening on ${PORT}'))
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
